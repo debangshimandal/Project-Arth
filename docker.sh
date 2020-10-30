5de9906b549b72ce4c6c2828900df3e0f286aa1a
Arnab Saha <42444789+Arnabsaha6@users.noreply.github.com>
2020-10-30 22:40:20 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -9,3 +9,13 @@ read newdir
 cmd="mkdir $newdir"
 eval $cmd
 #Dir Existence check
+#!/bin/bash
+echo -n "Enter directory name ->"
+read dir
+if [ -d "$dir" ]
+then
+echo "Directory exists"
+else
+`mkdir $dir`
+echo "Directory created"
+fi
