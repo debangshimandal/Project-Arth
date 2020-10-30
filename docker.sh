65a4766d8d200f06cb73c90e42a6e810489b6baf
Debangshi Mandal <70003464+debangshimandal@users.noreply.github.com>
2020-10-30 22:19:08 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,3 +2,4 @@ alias dc="docker-compose"
 alias dcr="docker-compose run --rm"
 #Remove dangling docker image
 alias dcdangling="docker rmi \$(docker images -f \"dangling=true\" -q)" 
+#Create directory script
